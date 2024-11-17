38b19b5550ba6f9332af97b5deb5ccdf4563e8ec
Irfan ahamed <irfansulaiha25@gmail.com>
2024-11-17 13:37:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+  async function handleDownloadPdf() {
+    try {
+      const response = await axios.get('/endpoint', {
+        responseType: 'json', 
+      });
+  
+      const fileUrl = response.data.fileUrl;
+  
+      const link = document.createElement('a');
+      link.href = fileUrl;
+      link.download = 'downloaded.pdf';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error('Error downloading file:', error);
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Button onClick={handleDownloadPdf}>Download PDF</Button>
   );
 }
 
